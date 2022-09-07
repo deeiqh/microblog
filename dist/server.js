f3413f6df1d6d52d9bf024ac50bebb1450a9a70c
Kevin Salazar <deeiqh@gmail.com>
2022-09-07 07:55:52 -0500

    chore: add prisma postgresql sample

--- dist/server.js
+++ dist/server.js
@@ -14,12 +14,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const swagger_ui_express_1 = require("swagger-ui-express");
+const client_1 = require("@prisma/client");
 const swagger_1 = require("./swagger");
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 const ENVIRONMENT = process.env.NODE_ENV || "development";
 app.use("/api-docs", swagger_ui_express_1.serve, (0, swagger_ui_express_1.setup)(swagger_1.documentation, { explorer: true }));
+const prisma = new client_1.PrismaClient();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const post = yield prisma.post.update({
+            where: { id: 1 },
+            data: {
+                published: true,
+            },
+        });
+        console.dir(post, { depth: null });
+    });
+}
+main()
+    .then(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$disconnect();
+}))
+    .catch((e) => __awaiter(void 0, void 0, void 0, function* () {
+    console.error(e);
+    yield prisma.$disconnect();
+    process.exit(1);
+}));
 app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`Server listening on port ${PORT}, env: ${ENVIRONMENT}`);
 }));
-//
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { serve, setup } from "swagger-ui-express";
+import { PrismaClient } from "@prisma/client";
 import { documentation } from "./swagger";
 
 const app = express();
@@ -7,6 +8,28 @@ const PORT = process.env.PORT || 3000;
 const ENVIRONMENT = process.env.NODE_ENV || "development";
 
 app.use("/api-docs", serve, setup(documentation, { explorer: true }));
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: {
+      published: true,
+    },
+  });
+  console.dir(post, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}, env: ${ENVIRONMENT}`);
